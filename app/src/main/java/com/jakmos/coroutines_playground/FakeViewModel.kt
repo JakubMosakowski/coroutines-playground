a3a4f7eef06d20446003a0ea5fc004255ccef6b1
jakubmosakowski <jmosakowski@spoton.com>
2022-07-07 10:22:07 +0200

    Prepare fake view model with it's own scope

---
+++ app/src/main/java/com/jakmos/coroutines_playground/FakeViewModel.kt
@@ -0,0 +1,41 @@
+package com.jakmos.coroutines_playground
+
+import android.util.Log
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
+
+class FakeViewModel {
+
+    private val scope = CoroutineScope(
+        Dispatchers.Default + SupervisorJob() + CoroutineExceptionHandler { coroutineContext, throwable ->
+            Log.v("KUBA", "Thrown: $throwable in context: $coroutineContext")
+        }
+    )
+
+    private val flow = (0..Int.MAX_VALUE)
+        .asSequence()
+        .asFlow()
+        .onEach { delay(1_000) }
+
+    fun collect() {
+        scope.launch {
+            Log.v("KUBA", "Start collecting")
+
+            flow.collect {
+                if(it == 5) throw Exception("Fake network error")
+                Log.v("KUBA", "Collected from shared flow: $it ---")
+            }
+        }
+    }
+
+    fun onDestroy() {
+        scope.cancel()
+    }
+}
--- app/src/main/java/com/jakmos/coroutines_playground/FirstFragment.kt
+++ app/src/main/java/com/jakmos/coroutines_playground/FirstFragment.kt
@@ -1,12 +1,11 @@
 package com.jakmos.coroutines_playground
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.navigation.fragment.findNavController
 import com.jakmos.coroutines_playground.databinding.FragmentFirstBinding
 
 /**
@@ -20,25 +19,32 @@ class FirstFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
-    private val viewModel: FirstViewModel by viewModels()
+    private var fakeViewModel: FakeViewModel = FakeViewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        viewModel.init()
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        binding.buttonStart.setOnClickListener {
+            fakeViewModel.collect()
         }
+
+        binding.buttonDestroy.setOnClickListener {
+            simulateDestroy()
+        }
+    }
+
+    private fun simulateDestroy() {
+        fakeViewModel.onDestroy()
+        fakeViewModel = FakeViewModel()
     }
 
     override fun onDestroyView() {
